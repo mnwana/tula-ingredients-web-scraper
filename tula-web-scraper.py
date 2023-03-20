3ae9d7132d2c5daa8d76cbbd9b67c4f40a7f0e5a
mnwana <mariellenwana@gmail.com>
2023-03-19 20:07:05 -0400

    create repo

---
+++ tula-web-scraper.py
@@ -0,0 +1,10 @@
+from selenium import webdriver
+
+
+
+
+# gather product list pages
+
+# for each page, get html / text from ingredients tab and clean it up. will collect page location and product name and cutesey name
+
+# store in json / csv / gsheet. future iterations will put it in a mongodb database

69083c18b2f4fa22ee1ef3ec73d71d8c570bfcef
mnwana <mariellenwana@gmail.com>
2023-03-20 00:34:28 -0400

    cleanup imports

--- tula-web-scraper.py
+++ tula-web-scraper.py
@@ -1,8 +1,6 @@
 from selenium import webdriver
-# from webdriver_manager.chrome import ChromeDriverManager
 from selenium.webdriver.common.by import By
 import pandas as pd
-import time
 
 driver = webdriver.Chrome()
 ingredientList = {}
